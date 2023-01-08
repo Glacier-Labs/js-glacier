1b0c74c888c4dc1cf618686dfe58062c7a4389c1
royweb <royme.chiang@gmail.com>
2023-01-08 12:19:01 +0800

    remove playground

--- apps/playground/src/libs/util.ts
+++
@@ -1,3 +0,0 @@
-export function shortAccount(account: string, length = 4) {
-  return account.slice(0, length + 2) + '...' + account.slice(-length)
-}
--- apps/playground/src/react-app-env.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference types="react-scripts" />
-
-declare const ethereum: import('@ethersproject/providers').ExternalProvider
